e34980235f4b0281349f160b1e0e0e3da59e53f5
DzmitryFrontier <76911669+DzmitryFrontier@users.noreply.github.com>
2021-01-03 18:46:57 +0300

    New site

---
+++ js/js.js
@@ -0,0 +1,10 @@
+"use strict";
+(function(){
+    burger_menu.onclick = function() {
+        burger_menu.classList.toggle("closer_burger");
+        nav.classList.toggle("nav_active");
+    }
+
+})(); 
+
+
